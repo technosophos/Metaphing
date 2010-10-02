ba4add45eeb74666014bc530bedddefaff779c01
Matt Butcher <technosophos@gmail.com>
2010-10-02 09:57:47 -0500

    Reorganizing code to better work on CLI.

---
+++ src/MetaPhingFoundation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This is the main class for the Fortissimo framework generator.
+ */
+class MetaPhingFoundation {
+  public static function getBuildXML() {
+    return dirname(__FILE__) . '/Fortissimo/fortissimo.xml';
+  }
+  
+  public static function getLibraryPath() {
+    return dirname(__FILE__);
+  }
+}
