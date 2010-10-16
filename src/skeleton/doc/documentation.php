9987584c698fa90c8501f90099028d50c71280de
Matt Butcher <technosophos@gmail.com>
2010-10-15 23:09:48 -0500

    Updated build docs.

--- src/skeleton/doc/documentation.php
+++ src/skeleton/doc/documentation.php
@@ -3,12 +3,12 @@
  * 
  * This is the main page for documentation.
  *
- * @subsection about_package About This Software
+ * @section about_package About This Software
  *
  * This is the programming reference for @@PROJECT@@. You will find complete documentation for 
  * the source of this package.
  *
- * @subsection learn_more Learn More
+ * @section learn_more Learn More
  *
  * You can learn more about this package at our website.
  */
